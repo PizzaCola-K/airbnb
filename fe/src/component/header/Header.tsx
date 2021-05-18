c48d7289274a3eb36d544ebbd98a13ed181cf6b0
kowoohyuk <kowoohyuk91@gmail.com>
2021-05-18 21:01:28 +0900

    [FE] chore: :gear: Search 작업 중
    - Header.jsx
    interSectionObserver 임시 작업 코드 있음!

--- fe/src/component/header/Header.tsx
+++ fe/src/component/header/Header.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { LoginButton } from './gnb/LoginButton';
 import { Logo } from './gnb/Logo';
 import { Navigation } from './gnb/Navigation';
 import { SearchBar } from './search-bar/SearchBar';
 
+interface isProps {
+  active: boolean;
+}
+
 export const Header = () => {
+  const [active, setActive] = useState(false);
+
+  const onIntersect: any = async ([entry]: any, observer: any) => {
+    console.log('entry', entry);
+    if (entry.isIntersecting) {
+      // observer.unobserve(entry.target);
+      // observer.observe(entry.target);
+    }
+  };
+
+  const target: any = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
+    observer.observe(target.current);
+    return () => observer.disconnect();
+  }, [target]);
+
   return (
-    <StyleHeader>
+    <StyleHeader active={active} ref={target}>
       <div className='global-navigation-bar'>
         <Logo />
         <Navigation />
@@ -21,6 +44,8 @@ const StyleHeader = styled.header`
   position: fixed;
   width: 100vw;
   padding: 1.25rem 5rem 1.25rem;
+  background-color: ${(props: isProps) =>
+    props.active ? '#fff' : 'transparent'};
   .global-navigation-bar {
     display: flex;
     justify-content: space-between;

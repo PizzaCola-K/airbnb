226515d66c7e40c5b934a11667361d83929f4a88
kowoohyuk <kowoohyuk91@gmail.com>
2021-05-18 15:01:40 +0900

    [FE] feat: :sparkles: 환경 설정 및 컴포넌트 구조 설계

---
+++ fe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from './component/header/Header';
+import { Main } from './component/main/Main';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;

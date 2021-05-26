240abdef33798e56cdfa2a10f74c01fa79f08233
JinseoPark <rarebook92@naver.com>
2021-05-26 12:02:58 +0900

    feat :sparkles: add hotelCell xib

---
+++ iOS/JacksonBnB/JacksonBnB/Controller/FoundHotelsByListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FoundHotelsViewController.swift
+//  JacksonBnB
+//
+//  Created by jinseo park on 5/26/21.
+//
+
+import Foundation
+import UIKit
+
+//리스트로 보여지거나 맵(FoundHotelsByMapViewController)으로 보여져야한다.
+class FoundHotelsByListViewController: UIViewController {
+    
+}
---
+++ iOS/JacksonBnB/JacksonBnB/View/HotelCell.swift
@@ -0,0 +1,17 @@
+//
+//  HotelCell.swift
+//  JacksonBnB
+//
+//  Created by jinseo park on 5/26/21.
+//
+
+import UIKit
+
+class HotelCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}

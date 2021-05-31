40f3716b5697c70646528fdc8120dba2a9bddf4a
K <PizzaCola.K@gmail.com>
2021-05-31 15:05:21 +0900

    feat: Reservation with builder pattern
    - 빌더 패턴으로 생성
    
    issue: #31

--- BE/src/main/java/codesquad/team17/gnb/reservation/model/Reservation.java
+++ BE/src/main/java/codesquad/team17/gnb/reservation/model/Reservation.java
@@ -3,15 +3,31 @@ package codesquad.team17.gnb.reservation.model;
 import java.time.LocalDate;
 
 public class Reservation {
-    private Long id;
-    private Long placeId;
-    private Long guestId;
-    private LocalDate checkIn;
-    private LocalDate checkOut;
-    private int adult;
-    private int child;
-    private int infant;
-    private int price;
+    private final Long id;
+    private final Long placeId;
+    private final Long guestId;
+    private final LocalDate checkIn;
+    private final LocalDate checkOut;
+    private final int adult;
+    private final int child;
+    private final int infant;
+    private final int price;
+
+    public Reservation(Builder builder) {
+        this.id = builder.id;
+        this.placeId = builder.placeId;
+        this.guestId = builder.guestId;
+        this.checkIn = builder.checkIn;
+        this.checkOut = builder.checkOut;
+        this.adult = builder.adult;
+        this.child = builder.child;
+        this.infant = builder.infant;
+        this.price = builder.price;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public Long getPlaceId() {
         return placeId;
@@ -43,5 +59,62 @@ public class Reservation {
 
     public int getPrice() {
         return price;
+    }
+
+    public static class Builder {
+        private Long id;
+        private Long placeId;
+        private Long guestId;
+        private LocalDate checkIn;
+        private LocalDate checkOut;
+        private int adult;
+        private int child;
+        private int infant;
+        private int price;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder placeId(Long placeId) {
+            this.placeId = placeId;
+            return this;
+        }
+
+        public Builder guestId(Long guestId) {
+            this.guestId = guestId;
+            return this;
+        }
+
+        public Builder checkIn(LocalDate checkIn) {
+            this.checkIn = checkIn;
+            return this;
+        }
+
+        public Builder checkOut(LocalDate checkOut) {
+            this.checkOut = checkOut;
+            return this;
+        }
+
+        public Builder adult(int adult) {
+            this.adult = adult;
+            return this;
+        }
+
+        public Builder child(int child) {
+            this.child = child;
+            return this;
+        }
+
+        public Builder infant(int infant) {
+            this.infant = infant;
+            return this;
+        }
+
+        public Builder price(int price) {
+            this.price = price;
+            return this;
+        }
     }
 }

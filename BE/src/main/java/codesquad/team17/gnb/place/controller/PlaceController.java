8d2199c3bb651f516565ac49df9fdfbbebaff584
K <PizzaCola.K@gmail.com>
2021-05-31 19:46:06 +0900

    feat: Change endpoint
    - endpoint 수정 (/api)
    
    issue: #31

--- BE/src/main/java/codesquad/team17/gnb/place/controller/PlaceController.java
+++ BE/src/main/java/codesquad/team17/gnb/place/controller/PlaceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/places")
+@RequestMapping("/api/places")
 public class PlaceController {
 
     private final PlaceService placeService;
--- BE/src/main/java/codesquad/team17/gnb/reservation/controller/ReservationController.java
+++ BE/src/main/java/codesquad/team17/gnb/reservation/controller/ReservationController.java
@@ -1,16 +1,13 @@
 package codesquad.team17.gnb.reservation.controller;
 
-import codesquad.team17.gnb.reservation.dto.ReservationDto;
 import codesquad.team17.gnb.reservation.dto.ReservationRequest;
 import codesquad.team17.gnb.reservation.model.Reservation;
 import codesquad.team17.gnb.reservation.service.ReservationService;
 import codesquad.team17.gnb.user.domain.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/reservation")
+@RequestMapping("/api/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;

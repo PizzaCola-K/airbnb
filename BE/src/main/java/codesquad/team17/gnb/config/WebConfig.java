a834f7463c7f41ec7171a1ae1f1841debc5fab3e
PizzaCola-K <PizzaCola.K@gmail.com>
2021-06-03 17:17:07 +0900

    fix: Add allowed Methods Options
    - preflight 를 위해, options 메소드 허용

--- BE/src/main/java/codesquad/team17/gnb/config/WebConfig.java
+++ BE/src/main/java/codesquad/team17/gnb/config/WebConfig.java
@@ -20,7 +20,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "HEAD", "POST", "OPTIONS")
+                .allowedOrigins("http://localhost:3000");
     }
 
     @Override
